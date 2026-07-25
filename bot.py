291ef75b6f8c8cc7e1d80906c3f1ecf0baf91928
dimapoberij3-maker <dimapoberij3@gmail.com>
2026-07-25 03:08:20 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ from aiohttp import web
 BOT_TOKEN = os.getenv("BOT_TOKEN") or "8963416771:AAHIlA7tiWh6e6fjNLqqkwBj_o2x8n8oBK0"
 CURRENT_VERSION = "1.0"
 
-DATABASE_URL = os.getenv("DATABASE_URL") or "postgresql://diams30690:6lw6qhN4oAiSgWyvVlA7DSDUi4ccvw56@dpg-d9hth27lk1mc738g881g-a/reaperdb"
+DATABASE_URL = os.getenv("DATABASE_URL") or "postgresql://dimas30690:YIWOeHx5ZcXoCyhpbpu3HhmbTwtHBcGe@dpg-d9hvs3naqgkc73c7nmc0-a/reaperdb_ac5j"
 
 ADMIN_TG_ID = 5541669577  
 # ==========================================================
